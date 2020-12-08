f5b219e54b7e953fa4604e3d42294202dde36fe7
carlijnweijer <carlijnweijer@gmail.com>
2020-12-08 10:28:17 +0100

    Add Note components

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,9 +1,15 @@
+import { Book, Code, Image, Note, NoteAdd } from "@material-ui/icons";
 import React from "react";
+import "./sidebar.scss";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <p>sidebar</p>
+      <NoteAdd />
+      <Note />
+      <Code />
+      <Book />
+      <Image />
     </div>
   );
 }
--- src/pages/NotebookPage/NotebookPage.js
+++ src/pages/NotebookPage/NotebookPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Sidebar from "../../components/Sidebar/Sidebar";
 
 export default function NotebookPage() {
   return (
-    <div>
+    <div className="notebook container">
       <h1>notebookpage</h1>
+      <Sidebar />
     </div>
   );
 }
