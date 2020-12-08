894794c428af263b19800274a1920906dca302cd
Sjouke91 <s.bosma1991@gmail.com>
2020-12-08 18:08:47 +0100

    Added an allUsers store to fetch all the users

--- src/store/rootreducer.js
+++ src/store/rootreducer.js
@@ -3,10 +3,12 @@ import appState from "./appState/reducer";
 import user from "./user/reducer";
 import allNotebooksReducer from "./myNotebooks/reducer";
 import allSubjectsReducer from "../store/subjects/reducer";
+import allUsersReducer from "../store/AllUsers/reducer";
 
 export default combineReducers({
   appState,
   user,
   notebooks: allNotebooksReducer,
   subjects: allSubjectsReducer,
+  allUsers: allUsersReducer,
 });
