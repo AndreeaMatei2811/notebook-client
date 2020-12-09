d932fd87efbec76e02ede21c97b8ede0721fbb96
Sjouke91 <s.bosma1991@gmail.com>
2020-12-09 12:38:07 +0100

    Added search functionality and refactored code

--- src/store/rootreducer.js
+++ src/store/rootreducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from "redux";
 import appState from "./appState/reducer";
 import user from "./user/reducer";
-import allNotebooksReducer from "./myNotebooks/reducer";
+import allNotebooksReducer from "./Notebooks/reducer";
 import allSubjectsReducer from "../store/subjects/reducer";
 import allUsersReducer from "../store/AllUsers/reducer";
 
