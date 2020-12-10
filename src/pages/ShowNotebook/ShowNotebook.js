7a4dfaa0d21ca3ab4443a499735445ab94f03464
Sjouke91 <s.bosma1991@gmail.com>
2020-12-10 20:05:19 +0100

    Added dropdown and redirect to shownotebook

--- src/pages/ShowNotebook/ShowNotebook.js
+++ src/pages/ShowNotebook/ShowNotebook.js
@@ -6,15 +6,32 @@ import NoteFeed from "./NoteFeed";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchSelectedNotebook } from "../../store/Notebooks/actions";
 import { selectNotebook } from "../../store/Notebooks/selectors";
-import { useParams } from "react-router-dom";
+import { useParams, useHistory } from "react-router-dom";
 import NoteEditor from "../../components/Notes/NoteEditor";
+import {
+  selectMyNotebooksIds,
+  selectMyNotebooks,
+} from "../../store/user/selectors";
+import { Select } from "@material-ui/core";
+import Fab from "@material-ui/core/Fab";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 
 export default function ShowNotebook() {
   const specificNotebook = useSelector(selectNotebook);
   const notebookId = parseInt(useParams().notebookId);
+  const ownedNotebookIds = useSelector(selectMyNotebooksIds);
+  const ownedNotebooks = useSelector(selectMyNotebooks);
   const [filter, set_filter] = useState("");
   const [filteredNotes, set_filteredNotes] = useState(specificNotebook.notes);
+  const [showDropdown, set_showDropdown] = useState(false);
   const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    if (ownedNotebookIds.includes(notebookId)) {
+      set_showDropdown(true);
+    }
+  }, [notebookId, ownedNotebookIds]);
 
   useEffect(() => {
     dispatch(fetchSelectedNotebook(notebookId));
@@ -32,6 +49,17 @@ export default function ShowNotebook() {
     set_filteredNotes(specificNotebook.notes);
   }, [filter, specificNotebook]);
 
+  //redirecting go back button
+  const onClickGoBack = () => {
+    if (showDropdown) {
+      history.push("/my-notebooks");
+    }
+    if (!showDropdown) {
+      history.push("/fellow-students");
+    }
+  };
+
+  console.log(showDropdown);
   //filter on date
   const sortedNotesByDate = filteredNotes?.sort((a, b) => {
     const dateA = new Date(a.createdAt);
@@ -44,6 +72,14 @@ export default function ShowNotebook() {
       <div className="side">
         <SidebarShowNotes setFilter={set_filter} notebookId={notebookId} />
       </div>
+      <Fab
+        style={{ position: "absolute", margin: "10px" }}
+        color="primary"
+        aria-label="add"
+        onClick={() => onClickGoBack()}
+      >
+        <ArrowBackIcon />
+      </Fab>
       <div style={{ width: " 100%" }}>
         <div className="main">
           <NotebookHeader
@@ -51,6 +87,26 @@ export default function ShowNotebook() {
             subheader={`Notebook owner: ${specificNotebook.user?.firstName} ${specificNotebook.user?.lastName}`}
           />
         </div>
+
+        {showDropdown ? (
+          <Select
+            variant="outlined"
+            fullWidth
+            id="demo-controlled-open-select"
+            color="primary"
+            onChange={(e) => history.push(`/show-notebook/${e.target.value}`)}
+          >
+            <option value="Select Subject"></option>
+            {ownedNotebooks.map((notebook) => {
+              return (
+                <option value={notebook.id} key={notebook.id}>
+                  {notebook.name}
+                </option>
+              );
+            })}
+          </Select>
+        ) : null}
+
         {filter === "showNoteEditor" ? (
           <NoteEditor />
         ) : (
